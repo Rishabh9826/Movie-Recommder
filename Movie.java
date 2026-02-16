08b68768a9fbb9792f730ad99b59b13adc46d919
Rishabh9826 <rishabhchoudhary7067@gmail.com>
2026-02-16 23:10:54 +0530

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,29 @@
+package com.movierecommender;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String genre;
+    private Integer year;
+    private Double rating;
+    private String description;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public Integer getYear() { return year; }
+    public void setYear(Integer year) { this.year = year; }
+    public Double getRating() { return rating; }
+    public void setRating(Double rating) { this.rating = rating; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
